782857145770bc9d0b08ecfb06d700dbe38ee430
Andres <andresftma@gmail.com>
2024-06-05 21:56:30 -0500

    probando

--- Carpeta Principal/src/Figuras/Figura_3D.java
+++ Carpeta Principal/src/Figuras/Figura_3D.java
@@ -1,4 +1,15 @@
 package Figuras;
 
-public class Figura_3D {
+public class Figura_3D extends Figuras_Geometricas{
+    public int nCaras;
+    public double arista;
+    public Figura_3D(int NLados,String nombre, int nCaras, double arista){
+        super(NLados,nombre);
+        this.nCaras=nCaras;
+        this.arista=arista;
+    }
+    @Override
+    public void mostrarInf(){
+        System.out.println("-Numero de caras: "+nCaras+"\n-Aristas: "+arista);
+    }
 }
