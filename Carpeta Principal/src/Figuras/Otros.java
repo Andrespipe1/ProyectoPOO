13e1fa028da2a0f8c3a4ca9858166cadf68dfd2c
GuamanFrancis <francisguaman909@gmail.com>
2024-06-06 07:47:55 -0500

    Update Otros.java
    Método creado para encontrar área y perímetro de un pentágono

--- Carpeta Principal/src/Figuras/Otros.java
+++ Carpeta Principal/src/Figuras/Otros.java
@@ -1,4 +1,31 @@
 package Figuras;
 
-public class Otros {
-}
+public class Otros extends Regulares {
+    public double apotema;
+
+    public Otros(int nlados, String nombre,double area,double perimetro, double lado, double apotema){
+        super(nlados,nlados,area,perimetro,lado);
+        this.apotema = apotema;
+
+
+    }
+
+    @Override
+    public double calcular_perimetro(){
+        return lado*nlados;
+
+    }
+
+    @Override
+    public double calcular_area(){
+        return (perimetro*apotema)/2;
+
+    }
+
+    @Override
+    public double mostrar_datos(){
+        mostrar_datos();
+        System.out.println("El area del "+nombre+" es de: "+calcular_area());
+        System.out.println("El perimetro del "+nombre+" es de: "+calcular_perimetro());
+
+    }
