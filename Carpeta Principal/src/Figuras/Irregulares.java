9f425473767e3906b7ab635ea21d10e4c9fa5952
GabrielSOA <150483291+GabrielSantiagoOAguilar@users.noreply.github.com>
2024-06-06 08:08:25 -0500

    Update Irregulares.java

--- Carpeta Principal/src/Figuras/Irregulares.java
+++ Carpeta Principal/src/Figuras/Irregulares.java
@@ -1,4 +1,17 @@
 package Figuras;
 
-public class Irregulares {
+public class Irregulares extends Figura_2D {
+
+    public String tipo;
+
+    // Constructor
+    public Regulares(int NLados,String nombre,double area, double perimetro, String tipo) {
+        super(NLados,nombre,area, perimetro);
+        this.tipo = tipo;
+    }
+  
+    @Override
+    public void mostrarInf() {
+        System.out.println(+"\n"+"Tipo: " + tipo);
+    }
 }
