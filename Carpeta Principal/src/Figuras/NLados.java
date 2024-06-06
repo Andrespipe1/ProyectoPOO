9bf567828ebb96fd36fb82749a7109d593082a33
GabrielSOA <150483291+GabrielSantiagoOAguilar@users.noreply.github.com>
2024-06-06 08:04:42 -0500

    Update NLados.java

--- Carpeta Principal/src/Figuras/NLados.java
+++ Carpeta Principal/src/Figuras/NLados.java
@@ -1,4 +1,20 @@
 package Figuras;
 
-public class NLados {
+class Nlados extends Otros {
+    public int cantidadLados;
+
+    // Constructor
+    public Nlados(double area, double perimetro, String tipo, String nombre, int cantidadLados) {
+        super(area, perimetro, tipo, nombre);
+        this.cantidadLados = cantidadLados;
+    }
+
+  
+    @Override
+    public double mostrar_datos(){
+        mostrar_datos();
+        System.out.println("La figura tiene: "+cantidadLados);
+        
+    }
+
 }
