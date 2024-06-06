9dd1707c5fd6af5249f71059ba1529d076f29470
GabrielSOA <150483291+GabrielSantiagoOAguilar@users.noreply.github.com>
2024-06-06 07:47:22 -0500

    Update Regulares.java

--- Carpeta Principal/src/Figuras/Regulares.java
+++ Carpeta Principal/src/Figuras/Regulares.java
@@ -1,4 +1,15 @@
 package Figuras;
 
-public class Regulares {
+class Regulares extends Figuras_2D {
+    public String tipo;
+
+    // Constructor
+    public Regulares(double area, double perimetro, String tipo) {
+        super(area, perimetro);
+        this.tipo = tipo;
+    }
+
+    public void imprimirTipo() {
+        System.out.println("Tipo: " + tipo);
+    }
 }
