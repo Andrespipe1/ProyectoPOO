f95a648d4afd4cc89165f00a9439eb7634fe6b04
GabrielSOA <150483291+GabrielSantiagoOAguilar@users.noreply.github.com>
2024-06-13 08:08:02 -0500

    Add files via upload

---
+++ Carpeta Principal/src/taller_S8/Evento.java
@@ -0,0 +1,19 @@
+public class Evento extends Reserva{
+    public String tipoEvento;
+
+    public Evento(int IdCliente,String fecha,double costoBase,String tipoEvento){
+        super(IdCliente,fecha, costoBase);
+        this.tipoEvento=tipoEvento;
+    }
+
+    @Override
+
+    private static void calcularCosto(){
+        //a=a+b
+    }
+
+    public static void MostrarInfo(){
+        //System.out.println("Costo Evento: "+a);
+    }
+
+}
