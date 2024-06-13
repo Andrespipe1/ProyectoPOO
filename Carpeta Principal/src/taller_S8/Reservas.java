05193f779f110f39eaf9d2d8086e98475427b04d
POO <andresftma@gmail.com>
2024-06-13 07:59:33 -0500

    taller s8

--- Carpeta Principal/src/taller_S8/Reservas.java
+++ Carpeta Principal/src/taller_S8/Reservas.java
@@ -1,4 +1,37 @@
 package taller_S8;
 
-public class Reservas {
+public class Reservas{
+    private int idCliente;
+    private String nombreCliente;
+    private String fecha;
+    private double costoBase;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public double getCostoBase() {
+        return costoBase;
+    }
+
+    public Reservas(int idCliente, String fecha,String nombreCliente,double costoBase){
+        this.idCliente=idCliente;
+        this.fecha=fecha;
+        this.nombreCliente=nombreCliente;
+        this.costoBase=costoBase;
+    }
+    public void calcularCosto(){
+        getCostoBase();
+    }
+    public void mostrarInfo(){
+        System.out.println("----RESERVACION----\nDatos cliente\nId:"+getIdCliente()+"Nombre: "+getNombreCliente()+"Fecha reserva: "+getFecha());
+    }
 }
