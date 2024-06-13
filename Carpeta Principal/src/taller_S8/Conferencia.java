ad0f942db627b6206c35e1bee2b0ce4231e89e63
POO <jair.vega@epn.edu.ec>
2024-06-13 08:38:22 -0500

    lol

--- Carpeta Principal/src/taller_S8/Conferencia.java
+++ Carpeta Principal/src/taller_S8/Conferencia.java
@@ -2,11 +2,15 @@ package taller_S8;
 public class Conferencia extends Evento{
     public int idConferencia;
     public String nombreConferencia;
+    public double catering;
+    public double live;
 
-    public Conferencia(int idCliente,String nombreCliente, String fecha,double costoBase,String tipoEvento,int idConferencia,String nombreConferencia){
+    public Conferencia(int idCliente,String nombreCliente, String fecha,double costoBase,String tipoEvento,int idConferencia,String nombreConferencia,double  catering, double live){
         super(idCliente,nombreCliente, fecha,costoBase,tipoEvento);
         this.idConferencia=idConferencia;
         this.nombreConferencia=nombreConferencia;
+        this.catering=catering;
+        this.live=live;
     }
 
     @Override
