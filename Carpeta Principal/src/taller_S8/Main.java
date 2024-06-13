e75cd79820c989ba80e765d14f7b72f3cd91ba1a
POO <andresftma@gmail.com>
2024-06-13 08:54:59 -0500

    taller s8

--- Carpeta Principal/src/taller_S8/Main.java
+++ Carpeta Principal/src/taller_S8/Main.java
@@ -1,4 +1,30 @@
 package taller_S8;
-
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args){
+        Scanner leer = new Scanner(System.in);
+        Bodas boda1=new Bodas(175427864,"Andres Galo","24/06/2024",100,"Boda",6321,56,500,300);
+        System.out.println("\tSISTEMA DE RESERVAS\t\nSeleccione un tipo de evento");
+        while(true) {
+            System.out.println("1) Conferencia\n2) Boda\n3) Concierto\n4)Salir\nIngrese una opcion: ");
+            int opcion = leer.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("--Evento: Conferencia\n");
+                    break;
+                case 2:
+                    System.out.println("--Evento: Boda\n");
+                    break;
+                case 3:
+                    System.out.println("--Evento: Concierto\n");
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar nuestro sistema :)");
+                    return;
+                default:
+                    System.out.println("Opción no válida. Por favor, elija una opción del menú.");
+            }
+        }
+
+    }
 }
